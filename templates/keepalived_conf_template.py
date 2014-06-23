79b8f9545deeff739ad22dd7e5c2d8a02e31a796
Ignatious Johnson <ijohnson@juniper.net>
2014-06-23 00:27:54 -0700

    fix the config filed for mysql database settings.

--- templates/keepalived_conf_template.py
+++ templates/keepalived_conf_template.py
@@ -8,10 +8,10 @@ vrrp_script chk_haproxy {
         weight  2
 }
 
-vrrp_instance OPENSTACK_HA_VIP {
+vrrp_instance OPENSTACK_HA_VIP_$__vip_str__ {
         interface $__device__
         state $__state__
-        virtual_router_id 71
+        virtual_router_id $__router_id__
         priority  $__priority__
         virtual_ipaddress {
                 $__virtual_ip__/$__virtual_ip_mask__ dev $__device__
